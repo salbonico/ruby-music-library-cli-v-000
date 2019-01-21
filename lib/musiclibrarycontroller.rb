27036cec399f39896863adea9e5353f092a1687c
Steve Albonico <salbonico@gmail.com>
2019-01-21 08:01:07 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -25,7 +25,7 @@ end
 
 def list_songs
   index = 1
-  sorted_array = Song.all.sort
+  sorted_array = Song.all.sort_by {|song| song.name}
 sorted_array.all.each do |song|
   puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
   index +=1
