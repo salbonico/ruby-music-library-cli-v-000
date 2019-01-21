74144e40f581dd3238332ab4d885dd160310a2ba
Steve Albonico <salbonico@gmail.com>
2019-01-21 06:55:22 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,10 +1,11 @@
 class Genre
 @@all = []
-
+extend Concerns::Findable
 attr_accessor :name
 
 def initialize(name)
   @name = name
+  @songs = []
 end
 
 
@@ -25,6 +26,20 @@ def self.create(name)
   new_object = self.new(name)
   new_object.save
   return new_object
+end
+
+def songs
+@songs
+end
+
+def artists
+output = []
+  self.songs.each do |song|
+    if !output.include?(song.artist)
+      output << song.artist
+    end
+  end
+return output
 end
 
 
